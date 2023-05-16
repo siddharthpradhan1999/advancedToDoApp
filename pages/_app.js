d3cdc5b99339fe4f20e7107e56a93fe0792bea1f
unknown <sid.sabir.pradhan@gmail.com>
2023-05-16 15:26:20 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <title>JS Dev - Todo App</title>
+                <title>SID - Todo App</title>
             </Head>
             <AuthUserProvider>
                 <Component {...pageProps} />
